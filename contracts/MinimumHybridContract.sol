57a97063fdc376dc569641bce754429f7c0f9bd9
jakobsn <jakob_sn_@hotmail.com>
2020-01-13 12:17:43 +0100

    Make necessary functions and tests. More docstrings

--- contracts/MinimumHybridContract.sol
+++ contracts/MinimumHybridContract.sol
@@ -1,68 +1,112 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
+/**
+ * @title Minimal Hybrid Contract
+ * @dev Proof of concept implementation of the proposed MHC
+ * @author Jakob Svennevik Notland
+ */
 
 contract MinimalHybridContract {
 
+    // Smart contract representation of a legal contract
     struct ContractStruct {
         address principal;
         address agent;
+        string title;
         string contract_hash;
         string contract_method;
     }
 
+    // Log event for contract creation
     event CreateContract (
-        address principal,
-        address agent,
+        address creator,
         uint contract_id
     );
 
-    // Keep all contracts in a list
-    ContractStruct[] contracts;
+    // Log event for contract transaction
+    event ContractTransaction (
+        uint contract_id,
+        string transaction_hash
+    );
 
+    // Keep all contract representations in a list
+    ContractStruct[] contracts;
     // Map actors to contracts
     mapping (address => uint[]) private contract_actor;
     // Map contract to transactions
     mapping (uint => string[]) private contract_transaction;
 
-    constructor () public {
-        return;
-    }
-
-    function create_contract(address principal, address agent, string calldata contract_hash, string calldata contract_method) external
+    /**
+    * @dev Create a contract struct and store it on chain. Anyone can create a contract,
+    * #TODO: further implementation should use multisignatures to ensure better security.
+    * This would be done by making it possible to make a contract struct only when both
+    * the principal and the agent signs for it.
+    * @param principal address of the participating principal
+    * @param agent address of the participating agent
+    * @param title overhead title of the legal contract
+    * @param contract_hash a hash generated from the legal contract
+    * @param contract_method the hashing algorithm used to generate the contract_hash
+    * @return {
+    *   "contract_id": "The id of the published contract_strct"
+    * }
+    */
+    function create_contract(address principal, address agent, string calldata title,
+            string calldata contract_hash, string calldata contract_method) external
         returns (uint contract_id) {
-        // Create a contract struct and store it on chain
+        // Make a ContractStruct instance contract_struct
         ContractStruct memory contract_struct;
         contract_struct.principal = principal;
         contract_struct.agent = agent;
+        contract_struct.title = title;
         contract_struct.contract_hash = contract_hash;
         contract_struct.contract_method = contract_method;
-
-        // Push the contract to the contracts list
-        contract_id = contracts.push(contract_struct);
-
-        // Map the actors to the contract
+        // Push the contract_struct to the contracts list
+        contract_id = contracts.push(contract_struct) - 1;
+        // Map the actors to the contract_struct
         contract_actor[principal].push(contract_id);
         contract_actor[agent].push(contract_id);
-
-        // Record metadata to the blockchain event log
-        emit CreateContract(principal, agent, contract_id);
+        // Record the contract creator and the contract id to the blockchain event log
+        emit CreateContract(msg.sender, contract_id);
         return contract_id;
     }
 
+    /**
+    * @dev Record a reference between transactions to their corresponding contract
+    * #TODO: Further implementations should allow only the sender to record their transaction.
+    * It should possible to record a transaction to no more than one contract id
+    * @param contract_id the id of the relevant contract struct
+    * @param transaction_hash the hash of a transaction related to the contract id
+    */
     function contract_transfer(uint contract_id, string calldata transaction_hash) external {
-        // TODO: Check if sender is actor in contract and transaction exists
+        // Store the contract id and transaction reference
         contract_transaction[contract_id].push(transaction_hash);
-        return;
+        // Record the event of a contract transaction
+        emit ContractTransaction(contract_id, transaction_hash);
     }
 
-    function read_contracts(address actor) external {
-        // TODO
-        return;
+    /**
+    * @dev Get the ids of the contracts related to an actor
+    * @param actor address of the actor
+    * @return {
+    *   "uint[]": "contract_ids"
+    * }
+    */
+    function read_contract_ids(address actor) external view
+        returns(uint[] memory contract_ids) {
+        return contract_actor[actor];
     }
 
-    function read_contract_transactions(uint contract_id) external {
-        // TODO
-        return;
+    /**
+    * @dev Get the transactions related to a contract
+    * @param contract_id id of the contract
+    * @return {
+    *   "string[]": "transactions"
+    * }
+    */
+    function read_contract_transactions(uint contract_id) external view
+        returns(string[] memory transactions) {
+        return contract_transaction[contract_id];
     }
 
 }
--- test/mhc.test.js
+++ test/mhc.test.js
@@ -1,30 +1,44 @@
 const MinimalHybridContract = artifacts.require("MinimalHybridContract")
 
-contract("KryptoSedler", (accounts) => { 
+contract("MinimalHybridContract", (accounts) => { 
 
     let mhc;
     let contract_id;
+    let transfer_tx;
 
-    it("Get contract", async () => { 
+    it("Get the deployed smart contract", async () => { 
         mhc = await MinimalHybridContract.deployed()
     })
 
-
-    it("Create binding contract", async () => { 
-        let contract_tx = await mhc.create_contract(accounts[0], accounts[1], "HASHDASDASDSSAD", "SHA256");
-        //console.log(contract_tx)
+    it("Store legal contract representation", async () => { 
+        let contract_tx = await mhc.create_contract(accounts[0], accounts[1], "Contract Title", "HASHDASDASDSSAD", "SHA256");
         contract_id = contract_tx.logs[0]["args"].contract_id;
-        //console.log("Contract id:", contract_id)
+        console.log("Legal contract id:", contract_id.toString())
+        expect(contract_id.toString()).to.equal('0')
     })
 
-    it("Send money and make reference to contract", async () => { 
-        let transfer_tx = await web3.eth.sendTransaction({from: accounts[1], to: accounts[0], gas: 210000, value: 10000})
+    it("Send transaction to be referenced to the contract", async () => { 
+        transfer_tx = await web3.eth.sendTransaction({from: accounts[1], to: accounts[0], gas: 210000, value: 10000})
+        console.log("Perform transaction:", transfer_tx["transactionHash"])
+    })
 
-        // Make for loop for each transaction
-        console.log(transfer_tx["transactionHash"])
+    it("Reference the transaction to the contract", async () => { 
         let contract_transfer_tx = await mhc.contract_transfer(contract_id, transfer_tx["transactionHash"])
-        console.log(contract_transfer_tx)
+        let event_contract_id = contract_transfer_tx.logs[0]["args"].contract_id;
+        console.log("Contract id linked to transaction hash")
+        expect(event_contract_id.toString()).to.equal('0')        
     })
 
+    it("Read contract ids", async () => { 
+        let ids = await mhc.read_contract_ids(accounts[1])
+        console.log("Contracts related to user:", ids.toString())
+        expect(ids.toString()).to.equal('0')
+    })
+
+    it("Read contract transactions", async () => { 
+        let transactions = await mhc.read_contract_transactions(contract_id)
+        console.log("Transactions related to contract id:", contract_id.toString(), "-", transactions)
+        expect(transactions[0]).to.equal(transfer_tx["transactionHash"])
+    })
 
 })
